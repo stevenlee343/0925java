c50d5450578e7951bb818fea4150c1bcfe1e8ca4
user <user@es302n18>
2018-09-25 11:50:01 +0800

    0925first

---
+++ 0925project/src/my/project/septproject.java
@@ -0,0 +1,17 @@
+package my.project;
+
+public class septproject {
+
+	public static void main(String[] args) {
+		int age, year;
+		year = 2000;
+		age = 19;
+		float weight = 12.5f;
+		age = year;
+		System.out.println(age);
+		System.out.println(year);
+		System.out.print(weight);
+		String school = "fju";
+	}
+
+}
